63a2be6df58d2cbfcc0a8f6004ed182cc8828f31
hermannramos <hermannhassai@gmail.com>
2025-08-30 13:38:06 +0200

    banner done

---
+++ src/app/components/BannerSlider.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { ChevronLeft, ChevronRight, Heart, Info } from "lucide-react";
+import "../../styles/banner-slider.scss";
+
+function getMovieImg(movie) {
+    return `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`;
+};
+
+export const BannerSlider = ({movies}) => {
+    const [slide, setSlide] = useState(0);
+
+    const nextSlide = () => {
+        setSlide(slide === movies.length - 1 ? 0 : slide + 1);
+    };
+
+    const prevSlide = () => {
+        setSlide(slide === 0 ? movies.length - 1 : slide - 1);
+    };
+
+    useEffect(() => {
+        const id = setInterval(() => setSlide(s => (s + 1) % movies.length), 6000);
+        return () => clearInterval(id);
+    }, [movies.length])
+
+    return (
+        <div className="banner-slider">
+            <ChevronLeft size={22} className="arrow arrow-left" onClick={prevSlide}/>
+           {movies.map((movie, index) => (
+            <div className={slide === index ? "slide" : " slide slide-hidden"} key={movie.id}>
+                <img src={getMovieImg(movie)} alt={movie.title}  className="img-banner"/>
+                <div className="banner-content">
+                    <h2>{movie.title}</h2>
+                    <p>{movie.overview}</p>
+                    <div className="button-actions">
+                        <button type="button"><Info size={18} style={{marginRight: 8}} />View Details</button>
+                        <button type="button"><Heart size={18} style={{marginRight: 8}} />Add To Wishlist</button>
+                    </div>
+                </div>
+            </div>
+           ))}
+           <ChevronRight size={22} className="arrow arrow-right" onClick={nextSlide}/>
+           <span className="indicators">
+            {movies.map((_, index) => (
+                <button key={index} onClick={() => setSlide(index)} className={slide === index ? "indicator" : "indicator indicator-inactive"}></button>
+            ))}
+           </span>
+        </div>
+    );
+};
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,18 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { BannerSlider } from "../components/BannerSlider.jsx";
 import { fetchPopularMovies, fetchPlayingNowMovies } from "../features/moviesThunks";
 import "../../styles/home.scss";
 
 const Home = () => {
     const dispatch = useDispatch();
-    // const popularMovies = useSelector((state) => state.movies.popularMovies);
     const nowPlayingMovies = useSelector((state) => state.movies.nowPlayingMovies);
-    const heroMovie = nowPlayingMovies && nowPlayingMovies[0];
-    const heroImage = 
-        heroMovie?.backdrop_path
-        ? `https://image.tmdb.org/t/p/w1280${heroMovie.backdrop_path}`
-        : (heroMovie?.poster_path ? `https://image.tmdb.org/t/p/w780${heroMovie.poster_path}` : '');
-
 
     useEffect(() => {
         dispatch(fetchPopularMovies(10));
@@ -21,18 +15,8 @@ const Home = () => {
 
     return (
         <div className="home-container">
-            {heroMovie && (
-                <section className="banner">
-                    <div className="banner-bg" style={{backgroundImage: `url(${heroImage})`}} />
-                    <div className="banner-content">
-                        <h1 className="banner-title">{heroMovie.title}</h1>
-                        <p className="banner-overview">{heroMovie.overview}</p>
-                        <div className="banner-actions">
-                            <button type="button">Ver detalles</button>
-                            <button></button>
-                        </div>
-                    </div>
-                </section>
+            {nowPlayingMovies.length > 0 && (
+                <BannerSlider movies={nowPlayingMovies} />
             )}
         </div>
     )
